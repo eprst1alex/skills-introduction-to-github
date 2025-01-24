e1177e48fe51bb8c4da6e91c5f4722f31156f0ba
eprst1alex <agrigor744@gmail.com>
2025-01-24 13:41:18 +0300

    chore: Add task ex2/ex3

---
+++ ex2.py
@@ -0,0 +1,6 @@
+seconds_per_hour=3600*24
+print("секунд в часе:",seconds_per_hour)
+seconds_per_day=seconds_per_hour*24
+print("секунд в сутках:",seconds_per_day)
+hour_per_day=seconds_per_day/seconds_per_hour
+print("часов в сутках:",hour_per_day)
